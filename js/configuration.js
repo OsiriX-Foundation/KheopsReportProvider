a140891b95078a0c8f78ab43a6dd20023dffe93b
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-06-12 15:11:38 +0200

    Update kheops configuration informations

--- js/configuration.js
+++ js/configuration.js
@@ -7,13 +7,19 @@ module.exports = {
       });
       return respJSON
     },
+    // https://tools.ietf.org/html/rfc7591
     JSON_kheopsConfiguration: function(scheme, host, port = '80') {
       var port = port === '80' || port === '443' ? '' : ':' + port
       var respJSON = JSON.stringify({
         'jwks_uri': `${scheme}://${host}${port}/certs`,
         'token_endpoint_auth_method': 'kheops_private_key_jwt',
         'token_endpoint_auth_signing_alg': 'RS256',
-        'redirect_uri': `${scheme}://${host}${port}/report.html`
+        'redirect_uri': `${scheme}://${host}${port}/report.html`,
+        'client_name': 'Kheops Report Provider',
+        'client_uri': 'https://kheops.online',
+        'contacts': [
+          'contact@kheops.online'
+        ]
       });
       return respJSON
     },
